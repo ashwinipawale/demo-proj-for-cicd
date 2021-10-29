fa761cd9f05e1ce7979724246f3211087167b594
Ashwini Chechar <ashwinipawale87@gmail.com>
2021-10-29 19:36:11 +0800

    added request handler demoMethod2

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -11,4 +11,9 @@ public class DemoController {
 	public ResponseEntity<String> demoMethod() {
 		return new ResponseEntity<String>("Test message from Demo Application", HttpStatus.OK);
 	}
+	
+	@GetMapping("/demo-test2")
+	public ResponseEntity<String> demoMethod2() {
+		return new ResponseEntity<String>("<h1>This is a test message from Demo application. demoMethod2</h1>", HttpStatus.OK);
+	}
 }
