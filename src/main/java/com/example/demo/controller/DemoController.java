f11023f15dd8903d04f4674fbaeac98dcdcee31e
ashwinipawale <ashwinipawale87@gmail.com>
2021-11-01 16:55:52 +0800

    test

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 	@GetMapping("/demo")
 	public ResponseEntity<String> demoMethod() {
-		return new ResponseEntity<String>("<h1>This is a test message from Demo application</h1>", HttpStatus.OK);
+		return new ResponseEntity<String>("<h1>This is the test message from Demo application</h1>", HttpStatus.OK);
 	}
 	
 }
